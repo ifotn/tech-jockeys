2cb90187ce269bc629d316db3621ded18d0eecd6
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-05-12 14:53:19 -0400

    l2/2 - create store controller

--- TechJockeys/Controllers/StoreController.cs
+++ TechJockeys/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechJockeys.Models;
 
 namespace TechJockeys.Controllers
 {
@@ -6,6 +7,31 @@ namespace TechJockeys.Controllers
     {
         public IActionResult Index()
         {
+            // fetch category data (mock data today, from db later)
+            var categories = new List<Category>();
+
+            // create mock category list
+            for (int i = 1; i < 16; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            // load view and pass the category list
+            return View(categories);
+        }
+
+        public IActionResult ByCategory(int id)
+        {
+            // error handle if id missing => redirect to Store index so user can choose a category
+            if (id == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // use id param to find category
+            // use ViewData dictionary to show selected category name in heading
+            ViewData["Category"] = "Category " + id.ToString();
+
             return View();
         }
     }
---
+++ TechJockeys/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace TechJockeys.Models
+{
+    public class Category
+    {
+        // using {ModelName}Id as the property tells the server automatically this is a PK
+        public int CategoryId { get; set; }
+
+       public string Name { get; set; }
+
+    }
+}
