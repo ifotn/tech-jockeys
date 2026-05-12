3c4d594bca695072d86c0e4fc3a0b17a5321cc35
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-05-12 13:54:21 -0400

    Add project files.

---
+++ TechJockeys/Controllers/StoreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechJockeys.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
