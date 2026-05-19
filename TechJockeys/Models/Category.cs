7bcce7b7ba008f441ee3941beaa76206ad806cd6
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-05-19 15:23:27 -0400

    l3/3 - Product model & relationship

--- TechJockeys/Models/Category.cs
+++ TechJockeys/Models/Category.cs
@@ -2,10 +2,12 @@
 {
     public class Category
     {
-        // using {ModelName}Id as the property tells the server automatically this is a PK
-        public int CategoryId { get; set; }
+       // using {ModelName}Id as the property tells the server automatically this is a PK
+       public int CategoryId { get; set; }
 
        public string Name { get; set; }
 
+       // child ref to products: 1 category - many products.  ref MUST BE nullable with ?
+       public List<Product>? Products { get; set; }
     }
 }
---
+++ TechJockeys/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace TechJockeys.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Description { get; set; }
+        public string? Image { get; set; }
+
+        // FK for parent CategoryId (required)
+        public int CategoryId { get; set; }
+
+        // Parent object ref.  We mark as nullable using ? for reasons we'll see later.
+        public Category? Category { get; set; }
+    }
+}
