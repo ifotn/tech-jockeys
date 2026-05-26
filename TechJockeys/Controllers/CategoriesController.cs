c59b58dc0f5e2ade7db8ed28ac05dcd83708876d
ifotn <Rich.Freeman@GeorgianCollege.ca>
2026-05-26 14:39:35 -0400

    l4/1 - create & read categories

--- TechJockeys/Controllers/CategoriesController.cs
+++ TechJockeys/Controllers/CategoriesController.cs
@@ -1,22 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechJockeys.Data;
 using TechJockeys.Models;
 
 namespace TechJockeys.Controllers
 {
     public class CategoriesController : Controller
     {
+        // shared db conn available to all controller methods
+        private readonly ApplicationDbContext _context;
+
+        // constructor w/dp dependency
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            // create empty Category list so view doesn't crash
-            var categories = new List<Category>();
+            // fetch Category list from db using DbSet object
+            var categories = _context.Category.ToList();
 
             // pass empty list to view
             return View(categories);
         }
 
+        // GET: /Categories/Create => show empty Category form
         public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: /Categories/Create => process form submission to create new Category
+        [HttpPost]
+        public IActionResult Create([Bind("Name")] Category category)
+        {
+            // validate input.  Show form again if invalid
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // create new category & save to db
+            _context.Category.Add(category);
+            _context.SaveChanges();
+
+            // redirect to Index to see updated list of Categories
+            return RedirectToAction("Index");
         }
 
         public IActionResult Edit()
--- TechJockeys/Data/Migrations/20260526174713_CreateCategoryAndProduct.Designer.cs
+++ TechJockeys/Data/Migrations/20260526174713_CreateCategoryAndProduct.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TechJockeys.Data;
 
@@ -11,9 +12,11 @@ using TechJockeys.Data;
 namespace TechJockeys.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260526174713_CreateCategoryAndProduct")]
+    partial class CreateCategoryAndProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
